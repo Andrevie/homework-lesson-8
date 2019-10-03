f67f46a26a382ca57716eac2f99d176c2f2ad374
André Kienast <andre.kienast@gmail.com>
2019-10-03 20:30:00 +0200

    Initial commit

---
+++ 1-python-homework-mood.py
@@ -0,0 +1,19 @@
+mood = "relaxed"
+
+if mood == "happy":
+    print("It is great to see you happy! ")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Dry your tears!")
+
+elif mood == "relaxed":
+    print("Good to be relaxed!")
+
+elif mood == "excited":
+    print("Jump up and down!")
+
+else:
+    print("I don't recognize this mood!")
